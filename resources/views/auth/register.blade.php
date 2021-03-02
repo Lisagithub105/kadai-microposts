a29156dc8806ba5c9dde8561169e3304a97252c7
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-02 06:18:24 +0000

    user login and fix regis

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
+    <div class="text-center">
         <h1>Sign up</h1>
     </div>
     <div class="row">
@@ -11,23 +11,23 @@
         
                 <div class="form-group">
                     
-                    {!! Form::label(['name', 'Name']) !!}
-                    {!! Form::text('name', old('name'), ['class' => 'form-control'] !!}
+                    {!! Form::label('name', 'Name') !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     
-                    {!! Form::label(['email', 'Email']) !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-control'] !!}
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     
-                    {!! Form::label(['password', 'Password']) !!}
-                    {!! Form::password('password', ['class' => 'form-control'] !!}
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     
-                    {!! Form::label(['password_confirmation', 'Confirmation']) !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-control'] !!}
+                    {!! Form::label('password_confirmation', 'Confirmation') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
                 
                 {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>Microposts</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no ">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 </head>
 <body>
