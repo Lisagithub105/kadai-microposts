d5a9a7c0eab23ef7d8764fb3b8fe7091c760280a
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-03 00:26:29 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) { //認証積みの場合
+            // 認証済のユーザを取得
+            $user = \Auther::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得
+            // （後のチャプターで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します。）
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+                ];
+        }
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
+    }
+    public function store(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            ]);
+            
+        // 認証済ユーザ（閲覧者）の投稿として作成（リクエストされた値をもとに作成）
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+        
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    public function destroy($id)
+    {
+        // idの値で投稿を検索して取得
+        $micropost = \App\Micropost::findOrFail($id);
+        
+        // 認証済ユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
+        if (\Auth::id() === $micropost->user_id)
+        {
+            $micropost->$delate();
+        }
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,9 +21,13 @@ class UsersController extends Controller
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
         
-        // ユーザ詳細ビューでそれを表示
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts()->orderBy('created_at', 'desc')->paginate(10);
+        
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
             ]);
     }
 }
